642588cb76f6aec6144aae07d370bdf1f1e03467
Void <51586851+Val0id@users.noreply.github.com>
2023-02-07 01:40:24 +0700

    ten

--- test.php
+++ test.php
@@ -1,3 +1,5 @@
 <!-- Simple PHP Backdoor By DK (One-Liner Version) -->
 <!-- Usage: http://target.com/simple-backdoor.php?cmd=cat+/etc/passwd -->
-<?php echo system("dir"); ?>
+<?php
+system("dir");
+?>
