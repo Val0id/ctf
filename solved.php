685fd2cb093d0d5911864f4333b108aea205e8d0
Void <51586851+Val0id@users.noreply.github.com>
2023-02-07 14:51:48 +0700

    Update and rename xxx.php to solved.php

---
+++ solved.php
@@ -0,0 +1,3 @@
+<?php
+print_r(readfile("/flag_341512514123213213"));
+?>
--- xxx.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$myfile = fopen("/flag_341512514123213213", "r") or die("Unable to open file!");
-echo fread($myfile,filesize("/flag_341512514123213213"));
-fclose($myfile);
-?>
