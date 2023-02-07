51d2445b6c4a723ef8cf1070d2fb1c1419d62f58
Void <51586851+Val0id@users.noreply.github.com>
2023-02-07 14:41:46 +0700

    Update and rename xxx.php to fsdaf.php

---
+++ fsdaf.php
@@ -0,0 +1,4 @@
+<?php
+require "./includes/header.inc.php";
+require "./includes/footer.inc.php";
+?>
--- xxx.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo exec("cat ./includes/header.inc.php");
-echo "\n";
-echo exec("cat ./includes/footer.inc.php");
-?>
