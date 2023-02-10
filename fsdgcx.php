68eb4979726123c291c34a4df5d5d0da88972571
Void <51586851+Val0id@users.noreply.github.com>
2023-02-11 04:48:39 +0700

    Update and rename testtest.php to fsdgcx.php

---
+++ fsdgcx.php
@@ -0,0 +1,4 @@
+<?php
+    $flag = "flag_341512514123213213";
+    echo readfile("php://filter/convert.base64-encode/resource=../../../" . $flag);
+?>
--- testtest.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $flag = "flag_341512514123213213";
-    echo phpinfo();
-?>
